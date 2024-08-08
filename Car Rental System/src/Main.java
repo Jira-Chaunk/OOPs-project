1f2100e87749418f584e223df189bb2269959443
Abhishek Panda <johnmactavish7298@gmail.com>
2024-08-08 23:39:55 +0530

    Car rental system is started

--- Car Rental System/src/Main.java
+++ Car Rental System/src/Main.java
@@ -90,5 +90,42 @@ class Rental{
         return days;
     }
 
+
+}
+
+class CarRentalSystem{
+
+    private List<Car> cars;
+    private List<Customer> customers;
+    private List<Rental> rentals;
+
+    public CarRentalSystem(){
+        cars = new ArrayList<>();
+        customers = new ArrayList<>();
+        rentals = new ArrayList<>();
+    }
+
+    public void addCar(Car car){
+        cars.add(car);
+    }
+
+    public void addCustomer(Customer customer){
+        customers.add(customer);
+    }
+
+    public void rentCar(Car car,Customer customer,int days){
+        if (car.isAvailable()) {
+            car.rent();
+            rentals.add(new Rental(car,customer,days));
+        } else {
+            System.out.println("Car is not available for rent");
+        }
+    }
+
+    public class Main {
     
+        public static void main(String[] args) {
+            
+        }
+    }
 }
